43ed824a8f41fe6c102319898db5975f76665208
Luis De Anda <ldeandac16@gmail.com>
2020-03-05 00:52:29 -0600

    Add file_name argument to constructor

--- ExcelPixelator.py
+++ ExcelPixelator.py
@@ -5,31 +5,32 @@ from PIL import Image
 
 
 class ExcelPixelator:
-    def __init__(self, image_path, scale):
-        self.image = Image.open(image_path).convert('RGB')
-        self.default_excel_font_size = 16
-        self.cell_size = 10  # size of the cell in pixels
+    def __init__(self, input_path, output_path, file_name, cell_size, scale=1):
+        self.image = Image.open(input_path).convert('RGB')
+        self.output_path = output_path
+        self.file_name = file_name
+        self.cell_size = cell_size  # size of the cell in pixels
         self.scale = scale  # not used yet
 
-    def create_pixel_art(self, output_path):
+    def create_pixel_art(self):
+        default_excel_font_size = 16
         wb = Workbook()
         ws = wb.create_sheet('Pixel-Art')
         wb.remove(wb['Sheet'])  # remove default worksheet
 
         width, height = self.image.size
         for _row in range(1, width):
-            ws.row_dimensions[_row].height = self.cell_size * \
-                10 / self.default_excel_font_size
+            ws.row_dimensions[_row].height = self.cell_size * 10 / default_excel_font_size
             for _col in range(1, height):
+                curr_col = get_column_letter(_col)
                 ws.column_dimensions[curr_col].width = self.cell_size / 9
 
-                curr_col = get_column_letter(_col)
                 rgbTuple = self.image.getpixel((_col, _row))
                 fill_color = self.__rgbToHex(rgbTuple)
                 ws[f'{curr_col}{_row}'].fill = PatternFill(
                     start_color=fill_color, end_color=fill_color, fill_type='solid')
 
-        wb.save(f'{output_path}.xlsx')
+        wb.save(f'{self.output_path}/{self.file_name}.xlsx')
         wb.close()
 
     def __rgbToHex(self, rgbTuple):
