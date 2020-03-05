5e1a9ef6a04d5b37c4710fd74f1a32e8de0054d1
Luis De Anda <ldeandac16@gmail.com>
2020-03-05 00:53:06 -0600

    Add command line args parsing logic

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import argparse
+import os
 from ExcelPixelator import ExcelPixelator
 
 if __name__ == '__main__':
-    p = ExcelPixelator('homer.jpg', 1)
-    p.create_pixel_art('ejemplo')
+    parser = argparse.ArgumentParser(
+        description='A simple script that takes an image and outputs Excel pixel art')
+    parser.add_argument('input', type=str,
+                        help='The path to the input image')
+    parser.add_argument('-o', '--output', type=str, default=f'{os.getcwd()}',
+                        help='Path where you want to save your Excel pixel art, if not provided default value will be the same path of this script')
+    parser.add_argument('-f', '--filename', type=str, default='Pixel Art',
+                        help='Name of the resulting Excel workbook, if not provided default value will be "Pixel Art"')
+    parser.add_argument('-c', '--cellsize', type=int, default=10,
+                        help='The size (width and height) in pixels of the cells where the image will be drawn, if not provided default value will be 10px')
+
+    args = parser.parse_args()
+
+    excelPixelator = ExcelPixelator(args.input, args.output, args.filename, args.cellsize)
+    excelPixelator.create_pixel_art()
